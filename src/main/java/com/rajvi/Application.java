6f686bdc04b1787205f461a09bd5d9588224127d
Pankaj Singh Bisht <psinghbisht@vmware.com>
2020-01-27 18:30:47 +0530

    Added Comment By Pankaj

--- src/main/java/com/rajvi/Application.java
+++ src/main/java/com/rajvi/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+    // Main Method
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
